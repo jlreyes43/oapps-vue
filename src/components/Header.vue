bedd1430c5fab73c01449f7ba44b91cb1d9ac191
JL Reyes <joseluisreyes43@gmail.com>
2020-07-03 12:06:53 +0800

    added some color to the header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1>Header Component</h1>
+  <div class="header-color">
+    <p class="text-light">OAPPS</p>
   </div>
 </template>
 
@@ -11,5 +11,8 @@ export default {
 </script>
 
 <style scoped>
-
+.header-color{
+  background-image: rgb(141,20,20);
+  background-image: linear-gradient(74deg, rgba(141,20,20,1) 0%, rgba(103,33,33,1) 100%);
+}
 </style>
