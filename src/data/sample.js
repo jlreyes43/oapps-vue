959058775eb2520e40f7792d317dd3a9abf11115
JL Reyes <joseluisreyes43@gmail.com>
2020-07-03 12:26:47 +0800

    created some sample data to play with

---
+++ src/data/sample.js
@@ -0,0 +1,42 @@
+const proposals = [
+    {
+        id: 1,
+        name: 'sample1',
+        org: {
+            name: 'BITS',
+            rep: 'Aethan',
+            contact: 'some contact',
+        },
+        nature: 'this is a very long string',
+        activityDate: 'some date',
+        submissionData: 'some date',
+        stage: {
+            treasurer: {
+                status: 'accepted',
+                date: 'some date'
+            },
+            secgen: {
+                status: 'accepted',
+                date: 'some date'
+            },
+            pres: {
+                status: 'accepted',
+                date: 'some date'
+            },
+            ap: {
+                status: 'accepted',
+                date: 'some date'
+            },
+            p: {
+                status: 'accepted',
+                date: 'some date'
+            },
+            d: {
+                status: 'accepted',
+                date: 'some date'
+            },
+        }
+    },
+    {},
+    {}
+];
