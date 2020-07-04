9470eedcafec86d90be3cd4f117e7fc94a19655d
JL Reyes <joseluisreyes43@gmail.com>
2020-07-04 13:31:56 +0800

    cleaned up the form page

--- src/components/proposal/Form.vue
+++ src/components/proposal/Form.vue
@@ -1,17 +1,12 @@
 <template>
     <div class="card col-10 m-5">
-        <p>Form page</p>
+        <h1>Form page</h1>
     </div>
 </template>
 
 <script>
 export default {
-    props:['proposal'],
-    methods:{
-        toProposalPage(){
-            this.$router.push({path: `/proposal/${this.proposal.id}`})
-        }
-    }
+
 }
 </script>
 
